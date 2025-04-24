9e5a96ea9a561f4b8859d8d8ac607a9e57806a40
Kintoyyy_Nabu_Tab <56209108+Kintoyyy@users.noreply.github.com>
2025-04-24 11:15:49 +0800

    nabu

--- includes/styles.php
+++ includes/styles.php
@@ -1,4 +1,4 @@
-<link href="https://cdn.jsdelivr.net/npm/halfmoon@2.0.2/css/halfmoon.min.css"
+<link href="<?php __DIR__ ?>/css/halfmoon.min.css"
     rel="stylesheet"
     integrity="sha256-RjeFzczeuZHCyS+Gvz+kleETzBF/o84ZRHukze/yv6o="
     crossorigin="anonymous">
--- public/residents/index.php
+++ public/residents/index.php
@@ -1,7 +1,5 @@
 <?php
-
 require __DIR__ . '/../../config/bootstrap.php';
-
 ?>
 
 <!doctype html>
@@ -21,34 +19,74 @@ require __DIR__ . '/../../config/bootstrap.php';
 <body>
     <?php include_once TEMPLATE_PATH . '/navbar.html'; ?>
 
-    <div id="resident-details"
-        class="mt-4 p-3 border rounded">
-        <p class="text-muted">Select a resident to view details</p>
-    </div>
+    <div class="container-lg">
+        <div id="resident-details"
+            class="mt-4 p-3 border rounded">
+            <p class="text-muted">Select a resident to view details</p>
+        </div>
 
-    <div id="residents-table">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Birth</th>
-                    <th>Gender</th>
-                    <th>Email</th>
-                    <th>Contact</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody hx-get="/api/residents"
-                hx-trigger="load">
-                <tr>
-                    <td colspan="5"
-                        class="text-center">Loading data...</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
 
+
+        <div class="row">
+            <div class="col-sm-6 mb-3 mb-sm-0">
+                <div class="card"
+                    style="width: 18.75rem;">
+                    <img src="..."
+                        class="card-img-top"
+                        alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </p>
+                        <a href="#"
+                            class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card"
+                    style="width: 18.75rem;">
+                    <img src="..."
+                        class="card-img-top"
+                        alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                        </p>
+                        <a href="#"
+                            class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="residents-table">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Birth</th>
+                        <th>Gender</th>
+                        <th>Email</th>
+                        <th>Contact</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody hx-get="/api/residents"
+                    hx-trigger="load">
+                    <tr>
+                        <td colspan="5"
+                            class="text-center">Loading data...</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </body>
 
 <?php include_once INCLUDES_PATH . '/scripts.php'; ?>
